98e531da42743c7ca8a5c5df8f9c436e3fe4c0c6
Maximilien Kpizingui <kpiz1@hotmail.com>
2024-05-04 17:24:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def main():
 
 
     st.set_page_config(page_title="Column Selector App with Sorting")
-    st.title("Column Selector App with Sorting")
+    st.title("Excel Column Explorer Application")
 
     # File upload widget with progress bar
     uploaded_file = st.file_uploader("Upload Excel File:", type="xlsx", on_change=show_progress_bar)
